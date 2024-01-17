3b5267c852c5e0278604ee7776e0220ed8ae038f
aoyulong <aoyulong@outlook.com>
2024-01-17 20:02:51 +0800

    Remove files temporarily before using git subtree

--- megatron/megatron/core/distributed/__init__.py
+++
@@ -1,2 +0,0 @@
-from .distributed_data_parallel import DistributedDataParallel
-from .finalize_model_grads import finalize_model_grads
--- megatron/tools/autoformat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# for now we just format core
-
-black ${SCRIPT_DIR}/../megatron/core
-isort ${SCRIPT_DIR}/../megatron/core
