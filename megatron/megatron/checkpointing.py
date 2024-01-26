1ae8f28902a78ac6a7286a4c524c3634c36ae979
ldwang <ftgreat@163.com>
2024-01-26 08:59:11 +0000

    Add add_bias_linear_qkv in checkpointing, loader_megatron and saver_megatron.
    Signed-off-by: ldwang <ftgreat@163.com>

--- megatron/megatron/checkpointing.py
+++ megatron/megatron/checkpointing.py
@@ -477,6 +477,8 @@ def load_args_from_checkpoint(args, load_arg='load'):
     # One-off conversion for foundation models
     if hasattr(checkpoint_args, 'disable_bias_linear'):
         setattr(checkpoint_args, 'add_bias_linear', not getattr(checkpoint_args, 'disable_bias_linear'))
+    if hasattr(checkpoint_args, 'disable_bias_linear_qkv'):
+        setattr(checkpoint_args, 'add_bias_linear_qkv', not getattr(checkpoint_args, 'disable_bias_linear_qkv'))
 
     # For backward compatibility.
     if hasattr(checkpoint_args, 'apply_layernorm_rms'):
@@ -511,6 +513,7 @@ def load_args_from_checkpoint(args, load_arg='load'):
     _set_arg('use_rotary_position_embeddings', force=True)
     _set_arg('rotary_percent', force=True)
     _set_arg('add_bias_linear', force=True)
+    _set_arg('add_bias_linear_qkv', force=True)
     _set_arg('swiglu', force=True)
     _set_arg('multiple_of', force=True)
     _set_arg('hidden_dim_multiplier', force=True)
