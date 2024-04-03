28277ea178455d44777b3bac7e48adbc2f4845b7
zhaoyinglia <ylzhao@baai.ac.cn>
2024-04-03 15:02:07 +0800

    rm comment

--- flagscale/train/train_aquila.py
+++ flagscale/train/train_aquila.py
@@ -2,11 +2,9 @@
 """Pretrain GPT."""
 
 import os
-# import sys
-# sys.path.append(os.path.dirname(
-#     os.path.dirname(os.path.abspath(__file__))))
-
-print("sys.path:", sys.path)
+import sys
+sys.path.append(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 import torch
 from functools import partial
