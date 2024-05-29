eab67e5fffaad9c9ec08bfba92ea234c0a8fd3b2
phoenixdong <liang_dong@foxmail.com>
2024-05-29 17:18:47 +0800

    pass tests/unit_tests/data

--- tests/scripts/unit_test_megatron.sh
+++ tests/scripts/unit_test_megatron.sh
@@ -1,11 +1,13 @@
 export PYTHONPATH=./megatron:$PYTHONPATH
 export PYTHONPATH=./../../FlagScale/:$PYTHONPATH
+
 cd megatron
-# torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/data
+
+torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/data
 torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/dist_checkpointing
 torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/fusions
 torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/models
 torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/pipeline_parallel
 torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/tensor_parallel
-torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/transformer
-torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/*.py
+torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/transformer 
+torchrun --nproc_per_node=8 -m pytest -q -x tests/unit_tests/*.py 
