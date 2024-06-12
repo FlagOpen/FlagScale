bf3bbad16f11ff91c763a48872b54b14addddacb
phoenixdong <liang_dong@foxmail.com>
2024-06-12 15:36:22 +0800

    add mixtral

--- tests/scripts/functional_test_flagscale.sh
+++ tests/scripts/functional_test_flagscale.sh
@@ -1,2 +1,5 @@
 python run.py --config-path tests/functional_tests/aquila/conf --config-name config action=test
 pytest -s tests/functional_tests/test_result.py --test_reaults_path=./tests/functional_tests/aquila/test_result
+
+python run.py --config-path tests/functional_tests/mixtral/conf --config-name config action=test
+pytest -s tests/functional_tests/test_result.py --test_reaults_path=./tests/functional_tests/mixtral/test_result
