760f69f9fe06b35cebe8e592b244d515f5992f89
shenzhu1993 <553525723@qq.com>
2024-06-24 11:05:09 +0000

    delete indent

--- megatron/megatron/core/transformer/transformer_block.py
+++ megatron/megatron/core/transformer/transformer_block.py
@@ -13,13 +13,12 @@ from megatron.core.dist_checkpointing.mapping import ShardedStateDict
 from megatron.core.dist_checkpointing.utils import replace_prefix_for_sharding
 from megatron.core.fusions.fused_layer_norm import FusedLayerNorm
 from megatron.core.packed_seq_params import PackedSeqParams
-
 try:
     from megatron.core.transformer.custom_layers.transformer_engine import (
         TEDelayedScaling,
         TENorm,
         get_cpu_offload_context,
-        te_checkpoint
+        te_checkpoint,
     )
 except:
     get_cpu_offload_context = None
