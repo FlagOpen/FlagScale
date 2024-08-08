76cb9657cf58c965fb92616b899eb0d7b76a0909
phoenixDong <liang_dong@foxmail.com>
2024-08-08 10:43:59 +0800

    Update test scripts, unit tests, and test environments

--- tests/scripts/unit_test_flagscale.sh
+++ tests/scripts/unit_test_flagscale.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -z "$1" ]
 then
   code_id=0
@@ -5,5 +7,14 @@ else
   code_id=$1
 fi
 
+run_pytest() {
+  local test_path=$1
+  echo "Running $test_path"
+  torchrun --nproc_per_node=8 -m pytest --import-mode=importlib --cov-append --cov-report=html:/workspace/report/$code_id/cov-report-flagscale --cov=flagscale -q -x -p no:warnings $test_path
+  if [ $? -ne 0 ]; then
+    echo "Pytest failed for $test_path"
+    exit 1
+  fi
+}
 
-torchrun --nproc_per_node=8 -m pytest --import-mode=importlib --cov-append --cov-report=html:/workspace/report/$code_id/cov-report-flagscale --cov=flagscale -q -x tests/unit_tests/launcher
+run_pytest tests/unit_tests/launcher
