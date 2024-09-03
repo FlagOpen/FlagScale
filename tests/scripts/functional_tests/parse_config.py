6f307e3a4c8474ad2f819867633d163d39f967bf
phoenixDong <42799804+phoenixdong@users.noreply.github.com>
2024-09-03 11:32:58 +0800

    [CI] Refactor the CI to be more modular and user-friendly (#199)
    ### Main features
    - Only run the latest commit of the user's PR.
    - Split unit tests by folder and functional tests by model into
    different jobs to avoid repeated runs when tests fail.
    - Use YAML files to control the content of unit and functional tests.
    - Before running the `pytest` command each time, check if the GPU is
    idle to avoid test failures caused by resource contention.
    
    ### How to run all unit tests locally
    ```bash
    tests/scripts/unit_tests/test_all.sh
    ```
    
    
    ### How to run specific unit tests locally
    ```bash
    tests/scripts/unit_tests/test_subset.sh --backend megatron --subset transformer
    ```
    
    
    ### How to add unit tests
    
    - **Without adding a new folder:**
      - No modification needed.
    
    - **When adding a new folder:**
    - Modify the `tests/scripts/unit_tests/config.yml` **configuration
    file**.
        
        **Configuration file structure**
    
        - **Framework name(megatron/flagscale/...):**
          - **set_environment:** Commands to configure the environment.
    - **coverage:** Directories that require test coverage statistics (the
    actual path = framework name + directory name).
      
        - **Subset name(models/transformer/...):**
    - **depth:** Specify which levels of tests to run. Default is to run all
    (`all`), also can be a integer.
          - **ignore:** Specify folders, files, or functions to skip.
    - **type:** Batch test or independent test. Default is batch (`batch`),
    also can be (`single`).
    
    **Note:** Default values do not need to be explicitly marked. If a
    folder uses all default configurations, it can be omitted from the
    configuration file.
        Here is an example of adding your own testing folder.
        ```
        flagscale:
          subset:
            your_folder:
              type: batch 
              depth: all 
              ignore: pytest_file.py
        ```
        How to run this example
        ```bash
    tests/scripts/unit_tests/test_subset.sh --backend flagscale --subset
    your_folder
        ```
    
    ### In the future, we will add unit testing and functional testing for
    VLLM

---
+++ tests/scripts/functional_tests/parse_config.py
@@ -0,0 +1,38 @@
+import yaml
+import argparse
+
+def parse_config(config_file, type, model):
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+
+    # Check if type exists in the configuration
+    if type not in config:
+        raise ValueError(f"Test type '{type}' not found in configuration file.")
+    
+    # Check if model exists within the specified type
+    if model not in config[type]:
+        raise ValueError(f"Test model '{model}' not found under test type '{type}' in configuration file.")
+
+    # Return the test cases for the specified test model under the test type
+    if "test_cases" in config[type][model]:
+        return config[type][model]['test_cases']
+
+    return []
+
+def main():
+    parser = argparse.ArgumentParser(description="Parse functional test configuration.")
+    parser.add_argument('--config', required=True, help="Path to the YAML configuration file.")
+    parser.add_argument('--type', required=True, help="Test type to run (e.g., 'train' or 'inference').")
+    parser.add_argument('--model', required=True, help="Test model to run (e.g., 'aquila' or 'mixtral').")
+
+    args = parser.parse_args()
+
+    try:
+        result = parse_config(args.config, args.type, args.model)
+        print(result)  
+    except ValueError as e:
+        print(str(e))
+        exit(1)
+
+if __name__ == "__main__":
+    main()
