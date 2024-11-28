96006cf1b60b0007545d8cb8d4cbc62ad1d83aec
Cathy Molisian <154107443+CathySama@users.noreply.github.com>
2024-11-28 10:35:56 +0800

    Delete flagscale/train directory

--- flagscale/train/__init__.py
+++
@@ -1,7 +0,0 @@
-from .global_vars import get_extra_valid_datasets
-from .global_vars import set_extra_valid_datasets
-from .global_vars import get_extra_input_tensor
-from .global_vars import set_extra_input_tensor
-from .global_vars import get_parallel_context
-from .global_vars import set_parallel_context
-from .arguments import FSTrainArguments
