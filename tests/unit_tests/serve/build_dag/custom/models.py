f16fa781312044d2714a848834f30759d505a85a
chenzhuo <chenzhuo@tju.edu.cn>
2024-12-26 10:34:51 +0800

    add case to build dag

---
+++ tests/unit_tests/serve/build_dag/custom/models.py
@@ -0,0 +1,11 @@
+def fn(input_data):
+    res = input_data + "__add_process_fn"
+    return res
+
+def model_C(input_data):
+    res = input_data + "__add_model_C"
+    return res
+
+def model_D(input_data_B, input_data_C):
+    output_data = input_data_B + input_data_C
+    return output_data
