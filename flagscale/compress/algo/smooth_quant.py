24e9ef878a63c57e7cbb1f77113bd91026256cd1
ceci3 <592712189@qq.com>
2025-01-08 10:15:02 +0800

    remove sq

--- flagscale/compress/algo/smooth_quant.py
+++
@@ -1,7 +0,0 @@
-from llmcompressor.modifiers.utils.compression_wrapper import ModuleCompressionWrapper
-from llmcompressor.modifiers.smoothquant import *
-
-class SmoothQuantWrapper(ModuleCompressionWrapper):
-    def __init__(self, name, layer):
-        super().__init__(name=name, layer=layer)
-        
