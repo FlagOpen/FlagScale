dae771ad94068f2fe17fefc47b120e50b995ff5d
chenzhuo <chenzhuo@tju.edu.cn>
2025-01-10 16:39:06 +0800

    polish code

--- flagscale/serve/run_serve.py
+++ flagscale/serve/run_serve.py
@@ -6,8 +6,6 @@ from flagscale.serve.core.dag import Builder
 
 def main():
     config = parse_config().serve
-    project_path = config["root_path"]
-    sys.path.append(project_path)
     builder = Builder(config)
     builder.build_task()
     builder.run_router_task()
