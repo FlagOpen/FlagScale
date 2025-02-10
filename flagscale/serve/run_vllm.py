4f215df8651b63d511b44fec7d60dfabc6e3e518
chenzhuo <chenzhuo@tju.edu.cn>
2025-02-10 09:59:57 +0800

    support multiple nodes

--- flagscale/serve/run_vllm.py
+++ flagscale/serve/run_vllm.py
@@ -53,6 +53,44 @@ def vllm_model(args):
     return process.returncode
 
 
+def vllm_multiple_nodes_serve(args):
+    vllm_args = args["serve"]["model_args"]["vllm_model"]
+
+    command = ["vllm", "serve"]
+    command.append(vllm_args["model-tag"])
+    for item in vllm_args:
+        if item not in {"model-tag", "action-args"}:
+            command.append(f"--{item}={vllm_args[item]}")
+    for arg in vllm_args["action-args"]:
+        command.append(f"--{arg}")
+
+    # Start the subprocess
+    logger.info(f"[Serve]: Starting vllm serve with command: {' '.join(command)}")
+    runtime_context = ray.get_runtime_context()
+    worker_id = runtime_context.get_worker_id()
+    job_id = runtime_context.get_job_id()
+    logger.info(
+        f"[Serve]: Current Job ID: {job_id} , \n[Serve]: ******** Worker ID: {worker_id} ********\n\n"
+    )
+    link_dir = os.path.join(
+        args.log_dir, f"session_latest_{timestamp}", f"worker-{worker_id}-"
+    )
+    logger.info(
+        f"\n\n[Serve]: **********************        {inspect.currentframe().f_code.co_name} Worker log path\
+        ********************** \n[Serve]: {link_dir} \n\n"
+    )
+
+    process = subprocess.Popen(command, stdout=sys.stdout, stderr=sys.stderr)
+    pid = os.getpid()
+    logger.info(f"[Serve]: Current vLLM PID: {pid} ")
+
+    stdout, stderr = process.communicate()
+    logger.info(f"[Serve]: Standard Output: {stdout}")
+    logger.info(f"[Serve]: Standard Error: {stderr}")
+
+    return process.returncode
+
+
 def main():
     # Note: Custom log dir here may cause "OSError: AF_UNIX path length cannot exceed 107 bytes:"
     ray.init(
