22afd7285b7d13f9070ba354310113b4e9b44c39
lxdcumt <1141051934@qq.com>
2025-02-17 14:37:42 +0800

    fix mtp bugs when using pp-tp

--- flagscale/train/train_deepseek_v3.py
+++ flagscale/train/train_deepseek_v3.py
@@ -152,6 +152,66 @@ def model_provider(pre_process=True, post_process=True) -> Union[GPTModel, megat
     return model
 
 
+
+def _get_batch_on_this_tp_rank(data_iterator):
+
+    args = get_args()
+
+    def _broadcast(item):
+       if item is not None:
+           torch.distributed.broadcast(item, mpu.get_tensor_model_parallel_src_rank(), group=mpu.get_tensor_model_parallel_group())
+
+    if mpu.get_tensor_model_parallel_rank() == 0:
+
+       if data_iterator is not None:
+           data = next(data_iterator)
+       else:
+           data = None
+
+       batch = {
+           'tokens': data["tokens"].cuda(non_blocking = True),
+           'labels': data["labels"].cuda(non_blocking = True),
+           'loss_mask': data["loss_mask"].cuda(non_blocking = True),
+           'attention_mask': None if "attention_mask" not in data else data["attention_mask"].cuda(non_blocking = True),
+           'position_ids': data["position_ids"].cuda(non_blocking = True)
+       }
+
+       _broadcast(batch['tokens'])
+       _broadcast(batch['labels'])
+       _broadcast(batch['loss_mask'])
+       _broadcast(batch['attention_mask'])
+       _broadcast(batch['position_ids'])
+
+    else:
+
+       tokens=torch.empty((args.micro_batch_size,args.seq_length), dtype = torch.int64 , device = torch.cuda.current_device())
+       labels=torch.empty((args.micro_batch_size,args.seq_length), dtype = torch.int64 , device = torch.cuda.current_device())
+       loss_mask=torch.empty((args.micro_batch_size,args.seq_length), dtype = torch.float32 , device = torch.cuda.current_device())
+       if args.create_attention_mask_in_dataloader:
+           attention_mask=torch.empty(
+                (args.micro_batch_size,1,args.seq_length,args.seq_length), dtype = torch.bool , device = torch.cuda.current_device()
+            )
+       else:
+           attention_mask=None
+       position_ids=torch.empty((args.micro_batch_size,args.seq_length), dtype = torch.int64 , device = torch.cuda.current_device())
+
+       _broadcast(tokens)
+       _broadcast(labels)
+       _broadcast(loss_mask)
+       _broadcast(attention_mask)
+       _broadcast(position_ids)
+
+       batch = {
+           'tokens': tokens,
+           'labels': labels,
+           'loss_mask': loss_mask,
+           'attention_mask': attention_mask,
+           'position_ids': position_ids
+       }
+
+    return batch
+
+
 def get_batch(data_iterator):
     """Generate a batch."""
 
@@ -160,7 +220,8 @@ def get_batch(data_iterator):
         return None, None, None, None, None
 
     # get batches based on the TP rank you are on
-    batch = get_batch_on_this_tp_rank(data_iterator)
+    # batch = get_batch_on_this_tp_rank(data_iterator)
+    batch = _get_batch_on_this_tp_rank(data_iterator)
 
     # slice batch along sequence dimension for context parallelism
     batch = get_batch_on_this_cp_rank(batch)
