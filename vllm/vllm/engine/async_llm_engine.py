c3678e9cbc859cc0cdaa3ad6e72f582d3ca6b155
kiddyjinjin <kiddyjinjin@gmailcom>
2025-02-17 01:51:29 +0000

    add FlagGems

--- vllm/vllm/engine/async_llm_engine.py
+++ vllm/vllm/engine/async_llm_engine.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import copy
 import time
@@ -648,6 +649,17 @@ class AsyncLLMEngine(EngineClient):
             usage_context=usage_context,
             stat_loggers=stat_loggers,
         )
+
+        if os.getenv("USE_FLAGGEMMS", "false").lower() in ("1", "true", "yes"):
+            try:
+                import flag_gems
+                flag_gems.enable()
+                logger.info("Successfully enabled flag_gems as default ops implementation.")
+            except ImportError:
+                logger.warning("Failed to import 'flag_gems'. Falling back to default implementation.")
+            except Exception as e:
+                logger.warning(f"Failed to enable 'flag_gems': {e}. Falling back to default implementation.")
+
         return engine
 
     @property
