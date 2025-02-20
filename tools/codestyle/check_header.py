c90c2ad42156b2c416419e7045c01261f607c661
zhaoyinglia <ylzhao@baai.ac.cn>
2025-02-20 15:27:39 +0800

    [Codestyle] update codestyle script and ci flow

---
+++ tools/codestyle/check_header.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2024, BAAI. All rights reserved.
+
+import argparse
+import datetime
+import os
+import re
+import sys
+
+COPYRIGHT = "# Copyright (c) {year}, BAAI. All rights reserved."
+
+RE_HEADER = re.compile(r".*Copyright \(c\) \d{4}", re.IGNORECASE)
+RE_ENCODE = re.compile(r"^[ \t\v]*#.*?coding[:=]", re.IGNORECASE)
+RE_SHEBANG = re.compile(r"^[ \t\v]*#[ \t]?\!")
+
+
+def _check_header(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            if not lines and RE_HEADER.search(line) is not None:
+                return True
+    return False
+
+
+def generate_header(path):
+    with open(path, 'r+', encoding='utf-8') as f:
+        lines = f.readlines()
+
+        insert_index = 0
+        for i, line in enumerate(lines[:5]):
+            if RE_ENCODE.search(line) or RE_SHEBANG.search(line):
+                insert_index = i + 1
+
+        f.seek(0, 0)
+        year = datetime.datetime.now().year
+        copyright = "".join([COPYRIGHT.format(year=year), os.linesep])
+
+        if insert_index == 0:
+            f.write(copyright + os.linesep)
+            f.writelines(lines)
+        else:
+            heads = lines[:insert_index]
+            f.write(heads)
+            f.write(copyright + os.linesep)
+            f.writelines(lines[insert_index:])
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description='Checker for header declaration.')
+    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    args = parser.parse_args(argv)
+
+    for path in args.filenames:
+        if _check_header(path):
+            continue
+
+        generate_header(path)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
