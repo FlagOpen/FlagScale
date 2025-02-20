c2df7d2b9116b0a728fbb27833ee9f2d833b93e2
zhaoyinglia <ylzhao@baai.ac.cn>
2025-02-20 16:13:23 +0800

    polish header

--- tools/codestyle/check_header.py
+++ tools/codestyle/check_header.py
@@ -3,8 +3,13 @@ import os
 import re
 import sys
 
-COPYRIGHT = "# Copyright (c) {year}, BAAI. All rights reserved."
-RE_HEADER = re.compile(r".*Copyright \(c\) \d{4}", flags=re.IGNORECASE)
+
+COPYRIGHT = '''Copyright © {year} BAAI. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License")'''
+
+
+RE_FS_HEADER = re.compile(r".*Copyright \(c\) \d{4}", flags=re.IGNORECASE)
 RE_ENCODE = re.compile(r"^[\t\v ]*#.*?coding[:=]", flags=re.IGNORECASE)
 RE_SHEBANG = re.compile(r"^[\t\v ]*#[ \t]?\!")
 
@@ -12,8 +17,10 @@ RE_SHEBANG = re.compile(r"^[\t\v ]*#[ \t]?\!")
 def _check_header(path):
     with open(path, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-        for line in lines:
-            if not lines and RE_HEADER.search(line) is not None:
+        for line in lines[:5]:
+            if not lines:
+                return False
+            if RE_FS_HEADER.search(line) is not None:
                 return True
     return False
 
@@ -29,7 +36,13 @@ def generate_header(path):
 
         f.seek(0, 0)
         year = datetime.datetime.now().year
-        copyright = "".join([COPYRIGHT.format(year=year), os.linesep])
+
+        copyright = ""
+        for l in COPYRIGHT.format(year=year).splitlines():
+            if l:
+                copyright += f"# {l}{os.linesep}"
+            else:
+                copyright += f"#{os.linesep}"
 
         if insert_index == 0:
             f.write(copyright + os.linesep)
