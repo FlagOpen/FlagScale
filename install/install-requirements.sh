0a59568aabda54f0371fd3be8ab851bf03f0c371
phoenixDong <liang_dong@foxmail.com>
2025-03-07 16:09:52 +0800

    tests/scripts/unit_tests/parse_config.py

--- install/install-requirements.sh
+++ install/install-requirements.sh
@@ -5,6 +5,7 @@ python -m pip install --upgrade setuptools
 
 # Packages that need to be installed outside of the conda environment
 pip install -r ../requirements/requirements-base.txt
+pip install -r ../requirements/requirements-lint.txt
 
 # Initialize the variable
 env=""
@@ -94,7 +95,6 @@ if [ "${env}" == "train" ]; then
     cd ..
     rm -r ./apex
 
-    pip install -r ../requirements/train/megatron/requirements-lint.txt
     python -m nltk.downloader -d /root/nltk_data punkt
 fi
 
