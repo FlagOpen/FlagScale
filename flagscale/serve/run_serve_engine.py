c4b9110e68ceba5709f16a4198dff9c2babd6546
chenzhuo <chenzhuo@tju.edu.cn>
2025-03-24 19:54:33 +0800

    fix code

--- flagscale/serve/run_serve_engine.py
+++ flagscale/serve/run_serve_engine.py
@@ -3,25 +3,33 @@ import os
 import subprocess
 import sys
 
-from flagscale import serve
-
 # Compatible with both command-line execution and source code execution.
 try:
     import flag_scale
 except Exception as e:
     pass
 
+from flagscale import serve
+from flagscale.serve.utils import flatten_dict_to_args
+
 
 def vllm_serve(args):
     vllm_args = args["serve"]["model_args"]["vllm_model"]
 
     command = ["vllm", "serve"]
-    command.append(vllm_args["model-tag"])
-    for item in vllm_args:
-        if item not in {"model-tag", "action-args"}:
-            command.append(f"--{item}={vllm_args[item]}")
-    for arg in vllm_args["action-args"]:
-        command.append(f"--{arg}")
+    if vllm_args.get("model-tag"):
+        command.append(vllm_args["model-tag"])
+        for item in vllm_args:
+            if item not in {"model-tag", "action-args"}:
+                command.append(f"--{item}={vllm_args[item]}")
+        for arg in vllm_args["action-args"]:
+            command.append(f"--{arg}")
+    elif vllm_args.get("model"):
+        command.append(vllm_args["model"])
+        other_args = flatten_dict_to_args(vllm_args, ["model-tag", "model"])
+        command.extend(other_args)
+    else:
+        raise ValueError("Either model-tag or model must be specified in vllm_model.")
 
     # Start the subprocess
     logger.info(f"[Serve]: Starting vllm serve with command: {' '.join(command)}")
--- flagscale/serve/utils.py
+++ flagscale/serve/utils.py
@@ -23,6 +23,27 @@ def load_once(func):
     return wrapper
 
 
+def flatten_dict_to_args(config_dict, ignore_keys=[]):
+    args = []
+    for key, value in config_dict.items():
+        if key in ignore_keys:
+            continue
+        key = key.replace("_", "-")
+        if isinstance(value, dict):
+            args.extend(flatten_dict_to_args(value, ignore_keys))
+        elif isinstance(value, list):
+            args.append(f"--{key}")
+            for v in value:
+                args.append(f"{v}")
+        elif isinstance(value, bool):
+            if value:
+                args.append(f"--{key}")
+        else:
+            args.append(f"--{key}")
+            args.append(f"{value}")
+    return args
+
+
 @load_once
 def load_args() -> None:
     """Load configuration for cluster init"""
