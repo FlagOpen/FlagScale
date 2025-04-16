314d41cb2b38a6535ac86d5bc6a9530ff7c257cf
chenzhuo <chenzhuo@tju.edu.cn>
2025-04-16 23:55:54 +0800

    fix code

--- flagscale/runner/runner_serve.py
+++ flagscale/runner/runner_serve.py
@@ -357,11 +357,11 @@ def _generate_run_script_serve(config, host, node_rank, cmd, background=True, wi
                     p_kv_config = {
                         "kv_connector": "P2pConnector",
                         "kv_role": "kv_producer",
-                        "kv_port": kv_port,
+                        "kv_port": str(kv_port),
                         "kv_connector_extra_config": {
                             "proxy_ip": master_ip,
-                            "proxy_port": kv_proxy_port,
-                            "http_port": http_port,
+                            "proxy_port": str(kv_proxy_port),
+                            "http_port": str(http_port),
                         },
                     }
                     card_ids = resource_manager.get_available_card_ids(
@@ -383,7 +383,7 @@ def _generate_run_script_serve(config, host, node_rank, cmd, background=True, wi
                         f.write(f"{ssh_cmd}\n")
                     else:
                         node_cmd = f"{ids_env} && {vllm_command} --port {http_port} --kv-transfer-config '{p_kv_config_json}'"
-                        f.write(f"{node_cmd}\n")
+                        f.write(f"{node_cmd} &\n")
 
                 for _ in range(d_num):
                     kv_port = kv_related_ports.pop()
@@ -391,11 +391,11 @@ def _generate_run_script_serve(config, host, node_rank, cmd, background=True, wi
                     d_kv_config = {
                         "kv_connector": "P2pConnector",
                         "kv_role": "kv_consumer",
-                        "kv_port": kv_port,
+                        "kv_port": str(kv_port),
                         "kv_connector_extra_config": {
                             "proxy_ip": master_ip,
-                            "proxy_port": kv_proxy_port,
-                            "http_port": http_port,
+                            "proxy_port": str(kv_proxy_port),
+                            "http_port": str(http_port),
                         },
                     }
                     card_ids = resource_manager.get_available_card_ids(
@@ -416,7 +416,7 @@ def _generate_run_script_serve(config, host, node_rank, cmd, background=True, wi
                         f.write(f"{ssh_cmd}\n")
                     else:
                         node_cmd = f"{ids_env} && {vllm_command} --port {http_port} --kv-transfer-config '{d_kv_config_json}'"
-                        f.write(f"{node_cmd}\n")
+                        f.write(f"{node_cmd} &\n")
 
             else:
                 f.write(f"ray_path=$(realpath $(which ray))\n")
