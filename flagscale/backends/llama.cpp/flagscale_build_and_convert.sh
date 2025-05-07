c8b04ff4d72fd558a3e2c1ee336b820ca2534856
hechenrui <hechenrui123@gmail.com>
2025-05-07 17:49:15 +0800

    update: build and convert script for  llama.cpp

---
+++ flagscale/backends/llama.cpp/flagscale_build_and_convert.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+print_help() {
+    echo "Example: $0 --llamma_cpp_backend cpu --model_path ./llama-7b-hf"
+    echo "Args:"
+    echo "  --llamma_cpp_backend   Assign backend (cpu, gpu, metal), cpu is the same with metal when build, default is cpu"
+    echo "  --model_path           Path to the model, HuggingFace or gguf format, default is ./model"
+}
+
+llamma_cpp_backend="cpu"
+model_path="./model"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            print_help
+            exit 0
+            ;;
+        --llamma_cpp_backend)
+            llamma_cpp_backend="$2"
+            shift 2
+            ;;
+        --model_path)
+            model_path="$2"
+            shift 2
+            ;;
+        *)
+            echo "unknown arg: $1"
+            shift
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
+echo $0 "Args:"
+echo "------------------------"
+echo "llamma_cpp_backend:" ${llamma_cpp_backend}
+echo "model_path        :" ${model_path}
+echo "------------------------"
+
+# CLEAN
+rm -rf ./build
+rm -rf ${model_path}/ggml_model_f16.gguf
+# BUILD
+./build.sh ${llamma_cpp_backend} 
+# CONVERT
+./convert.sh ${model_path}
---
+++ flagscale/backends/llama.cpp/flagsclae_convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+print_help() {
+    echo "Example: $0 <model_path>"
+    echo "Args:"
+    echo "  model_path: Path to the model, HuggingFace format."
+}
+
+if [ $# -ne 1 ]; then
+    print_help; exit 1;
+fi
+
+python ../convert_hf_to_gguf.py $1 --outfile $1/ggml_model_f16.gguf
+echo "Convert $1 to gguf format ($1/ggml_model_f16.gguf.) successfully."
