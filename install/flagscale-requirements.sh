30254d85ccefdb7092dc202e1690c6b36a895449
yefubao <fbye@baai.ac.cn>
2025-06-30 09:28:45 +0800

    update Dockerfile.reduce.cuda

--- install/flagscale-requirements.sh
+++ install/flagscale-requirements.sh
@@ -63,7 +63,7 @@ conda activate flagscale
 # pip install --upgrade setuptools
 
 # # Navigate to requirements directory and install basic dependencies
-# pip install -r ./requirements/requirements-common.txt
+# pip install --no-cache-dir -r ./requirements/requirements-common.txt
 
 # # TransformerEngine
 # # Megatron-LM requires TE >= 2.1.0.
@@ -73,7 +73,7 @@ conda activate flagscale
 #     git clone --recursive https://github.com/NVIDIA/TransformerEngine.git
 #     cd TransformerEngine
 #     git checkout 5bee81e
-#     pip install .
+#     pip install --no-cache-dir .
 #     cd ..
 #     rm -r ./TransformerEngine
 # fi
@@ -83,7 +83,7 @@ conda activate flagscale
 #     pip show nvidia-cudnn-cu12
 
 # else  
-#     pip install nvidia-cudnn-cu12==9.7.1.26
+#     pip install --no-cache-dir nvidia-cudnn-cu12==9.7.1.26
 #     CMAKE_ARGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5" pip install nvidia-cudnn-frontend
 #     python -c "import torch; print('cuDNN version:', torch.backends.cudnn.version());"
 #     python -c "from transformer_engine.pytorch.utils import get_cudnn_version; get_cudnn_version()"
@@ -98,7 +98,7 @@ conda activate flagscale
 #     cp=$(python3 --version | awk '{print $2}' | awk -F. '{print $1$2}')
 #     cxx=$(g++ --version | grep 'g++' | awk '{print $3}' | cut -d '.' -f 1)
 #     wget https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.0.post2/flash_attn-2.8.0.post2+cu${cu}torch${torch}cxx${cxx}abiFALSE-cp${cp}-cp${cp}-linux_x86_64.whl
-#     pip install flash_attn-2.8.0.post2+cu${cu}torch${torch}cxx${cxx}abiFALSE-cp${cp}-cp${cp}-linux_x86_64.whl
+#     pip install --no-cache-dir flash_attn-2.8.0.post2+cu${cu}torch${torch}cxx${cxx}abiFALSE-cp${cp}-cp${cp}-linux_x86_64.whl
 #     rm flash_attn-2.8.0.post2+cu${cu}torch${torch}cxx${cxx}abiFALSE-cp${cp}-cp${cp}-linux_x86_64.whl
 # fi
 
@@ -106,7 +106,7 @@ conda activate flagscale
 # if pip list | grep -q flashattn-hopper;then
 #     pip show flashattn-hopper
 # else
-#     pip install "git+https://github.com/Dao-AILab/flash-attention.git@v2.7.2#egg=flashattn-hopper&subdirectory=hopper"
+#     pip install --no-cache-dir "git+https://github.com/Dao-AILab/flash-attention.git@v2.7.2#egg=flashattn-hopper&subdirectory=hopper"
 #     python_path=`python -c "import site; print(site.getsitepackages()[0])"`
 #     mkdir -p $python_path/flashattn_hopper
 #     wget -P $python_path/flashattn_hopper https://raw.githubusercontent.com/Dao-AILab/flash-attention/v2.7.2/hopper/flash_attn_interface.py
