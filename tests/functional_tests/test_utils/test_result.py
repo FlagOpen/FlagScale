5a3722051956ade2c664fd3b1e6a58ba7660cbeb
zihugithub <103628180+zihugithub@users.noreply.github.com>
2025-07-10 10:07:41 +0800

    [CICD][Fix] Update inference testing (#654)
    1. **Update Workflows**
    **All-tests.yml**: Cancel the annotation of the inference testing
    module.
    
    2. Addressing the randomness issue in inference
    **test_result.py**: Resolve the issue of randomly generating '\ n' when
    reading lines to process data
    
    ---------
    
    Co-authored-by: yefubao <fbye@baai.ac.cn>

--- tests/functional_tests/test_utils/test_result.py
+++ tests/functional_tests/test_utils/test_result.py
@@ -193,3 +193,56 @@ def test_inference_pipeline(test_path, test_type, test_task, test_case):
             or (result_value[0] == "(" and result_value[-1] == ")")
             and result_value[1:-1]
         ), f"String {result_value} should be wrapped in [] or () and not empty inside"
+
+
+@pytest.mark.usefixtures("test_path", "test_type", "test_task", "test_case")
+def test_rl_equal(test_path, test_type, test_task, test_case):
+    # Construct the test_result_path using the provided fixtures
+    test_result_path = os.path.join(test_path, test_type, test_task, "results_test", test_case)
+    result_path = os.path.join(test_result_path, "logs/host_0_localhost.output")
+
+    print("result_path:", result_path)
+
+    assert os.path.exists(result_path), f"Failed to find 'host_0_localhost.output' at {result_path}"
+
+    with open(result_path, "r") as file:
+        lines = file.readlines()
+
+    result_json = {}
+    result_json["val-core/openai/gsm8k/reward/mean@1"] = []
+
+    for line in lines:
+        if "step" in line:
+            line_split = line.strip().split(" ")
+            for key_value in line_split:
+                if key_value.startswith("val-core/openai/gsm8k/reward/mean"):
+                    # Extract the value after the colon
+                    value = key_value.split(":")[-1]
+                    # Convert to float and append to the list
+                    result_json["val-core/openai/gsm8k/reward/mean@1"].append(float(value))
+
+    gold_value_path = os.path.join(
+        test_path, test_type, test_task, "results_gold", test_case + ".json"
+    )
+    assert os.path.exists(gold_value_path), f"Failed to find gold result JSON at {gold_value_path}"
+
+    with open(gold_value_path, "r") as f:
+        gold_result_json = json.load(f)
+
+    print("\nResult checking")
+    print("Result: ", result_json)
+    print("Gold Result: ", gold_result_json)
+    print(
+        "The results are basically equal: ",
+        np.allclose(
+            gold_result_json["val-core/openai/gsm8k/reward/mean@1"],
+            result_json["val-core/openai/gsm8k/reward/mean@1"],
+            atol=0.05,
+        ),
+    )
+
+    assert np.allclose(
+        gold_result_json["val-core/openai/gsm8k/reward/mean@1"],
+        result_json["val-core/openai/gsm8k/reward/mean@1"],
+        atol=0.05,
+    ), "Result not close to gold result"
