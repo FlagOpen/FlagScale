90fa250426f5a70b81a067ef6508ee0857d28c02
caozhou <caozhou@baai.ac.cn>
2025-07-22 20:27:06 +0800

    install metax

--- version.py
+++ version.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import subprocess
 import sys
@@ -11,7 +12,8 @@ BACKEND_DEVICE_VERSION = {
         "gpu": [
             "0.9.2.dev0+gb6553be1b.d20250708.cu124-cp310-cp310-linux_x86_64",
             "0.9.2.dev0+gb6553be1b.d20250708.cu124-cp312-cp312-linux_x86_64",
-        ]
+        ],
+        "metax": ["0.7.2+maca2.29.2.7torch2.4-cp310-cp310-linux_x86_64"],
     }
 }
 
@@ -19,6 +21,8 @@ BACKEND_DEVICE_VERSION = {
 def get_whl_version(backend, device):
     if device == "gpu":
         return _get_whl_version_gpu(backend)
+    elif device == "metax":
+        return _get_whl_version_metax(backend)
     else:
         raise NotImplementedError(f"Unsupported device type: {device}")
 
@@ -48,7 +52,44 @@ def _get_whl_version_gpu(backend):
 
     if backend == "vllm":
         suffix = f"{cuda_version}-{python_version}-{python_version}-{current_platform_tag}"
-        versions = BACKEND_DEVICE_VERSION["vllm"]["gpu"]
+        versions = BACKEND_DEVICE_VERSION[backend]["gpu"]
+        compatible_versions = []
+        for version in versions:
+            if version.endswith(suffix):
+                compatible_versions.append(version)
+        return versions, compatible_versions
+    else:
+        raise NotImplementedError(f"Unsupported backend: {backend}")
+
+
+def _get_whl_version_metax(backend):
+    def get_maca_version():
+        default_version = "2.29.2.7"
+        maca_path = os.getenv('MACA_PATH')
+        if not os.path.exists(maca_path):
+            return default_version
+        else:
+            version_file = os.path.join(maca_path, 'Version.txt')
+            if not os.path.exists(version_file) or not os.path.isfile(version_file):
+                return default_version
+            with open(version_file, 'r') as f:
+                version = f.readline().strip().split(":")[-1]
+                return version
+
+    maca_version = get_maca_version()
+    import torch
+
+    torch_version = torch.__version__
+    torch_version = ".".join(torch_version.split(".")[:2])
+    metax_version = "maca" + maca_version + "torch" + torch_version
+    python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
+    platform_str = platform.system().lower()  # 'linux' / 'darwin' / 'windows'
+    arch = platform.machine().lower()  # 'x86_64' / 'aarch64' / etc.
+    current_platform_tag = f"{platform_str}_{arch}"
+
+    if backend == "vllm":
+        suffix = f"{metax_version}-{python_version}-{python_version}-{current_platform_tag}"
+        versions = BACKEND_DEVICE_VERSION[backend]["metax"]
         compatible_versions = []
         for version in versions:
             if version.endswith(suffix):
