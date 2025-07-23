4828141c42ae85cfccd7812065b31816f3d2507e
yefubao <fbye@baai.ac.cn>
2025-07-23 16:44:33 +0800

    Add inference testing for Muxi chip

--- tests/scripts/functional_tests/test_all.sh
+++ tests/scripts/functional_tests/test_all.sh
@@ -23,8 +23,12 @@ commands=(
     # "tests/scripts/functional_tests/test_task.sh --type inference --task Qwen3-4B --hardware bi_v150 --flaggems enable --pipeline true"
     "tests/scripts/functional_tests/test_task.sh --type inference --task Qwen3-4B --hardware cambricon_mlu --pipeline true"
     # "tests/scripts/functional_tests/test_task.sh --type inference --task Qwen3-4B --hardware cambricon_mlu --flaggems enable --pipeline true"
+    "tests/scripts/functional_tests/test_task.sh --type inference --task Qwen3-4B --hardware metax --pipeline true"
+    # "tests/scripts/functional_tests/test_task.sh --type inference --task Qwen3-4B --hardware metax --flaggems enable --pipeline true"
     # For serve
     # "tests/scripts/functional_tests/test_task.sh --type serve --task base"
+    # For rl
+    "tests/scripts/functional_tests/test_task.sh --type rl --task qwen2_5"
 )
 
 for cmd in "${commands[@]}"; do
--- tests/scripts/functional_tests/test_task.sh
+++ tests/scripts/functional_tests/test_task.sh
@@ -132,7 +132,7 @@ hardware="nvidia"
 pipeline="false"
 
 # Define supported hardware options in a list (array)
-supported_hardware=("nvidia" "bi_v150" "cambricon_mlu")
+supported_hardware=("nvidia" "bi_v150" "cambricon_mlu" "metax")
 
 # Parse command-line arguments
 while [[ "$#" -gt 0 ]]; do
