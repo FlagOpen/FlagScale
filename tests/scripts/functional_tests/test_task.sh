ce12f6cabe502be194afbd059d604331e664d786
chenzhuo <chenzhuo@tju.edu.cn>
2025-08-14 17:08:34 +0800

    polish test case 7

--- tests/scripts/functional_tests/test_task.sh
+++ tests/scripts/functional_tests/test_task.sh
@@ -74,7 +74,6 @@ test_task() {
         run_command "pytest -s tests/functional_tests/test_utils/test_result.py::test_inference_equal --test_path=tests/functional_tests/test_cases --test_type=${_type} --test_task=${_task} --test_case=${_case}" $attempt_i $_task $_type $_case
       fi
 
-      # todo: open this case
       if [ "${_type}" = "serve" ]; then
         run_command "python run.py --config-path tests/functional_tests/test_cases/${_type}/${_task}/conf --config-name ${_case} action=run; sleep 1m" $attempt_i $_task $_type $_case
         run_command "pytest -s tests/functional_tests/test_utils/test_result.py::test_serve_equal --test_path=tests/functional_tests/test_cases --test_type=${_type} --test_task=${_task} --test_case=${_case}" $attempt_i $_task $_type $_case
