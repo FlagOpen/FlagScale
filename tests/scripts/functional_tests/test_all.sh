fc991492be12444e3c4b10ea9d28768a80165909
yefubao <fbye@baai.ac.cn>
2025-08-18 12:04:23 +0800

    update workflows

--- tests/scripts/functional_tests/test_all.sh
+++ tests/scripts/functional_tests/test_all.sh
@@ -13,7 +13,7 @@ commands=(
     "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen"
     "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-flaggems"
     "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-metax"
-    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-metax-flaggems"
+    "tests/scripts/functional_tests/test_task.sh --type inference --task deepseek_r1_distill_qwen-flaggems-metax"
     # For serve
     # "tests/scripts/functional_tests/test_task.sh --type serve --task base"
 )
