7b4f0375b13319b2c3f9f0e9ba7d9dc172cf5a6f
shuailong616 <452509829@qq.com>
2025-09-02 11:06:09 +0800

    activate test

--- flagscale/runner/runner_train.py
+++ flagscale/runner/runner_train.py
@@ -313,9 +313,9 @@ class SSHTrainRunner(RunnerBase):
         assert self.task_type == "train", f"Unsupported task type: {self.task_type}"
         self._prepare()
         master_port = getattr(self.config.experiment.runner, "master_port", None)
-        self.random_no_use_port(master_port)
+        self.random_one_no_use_port(master_port)
 
-    def random_no_use_port(self, port):
+    def random_one_no_use_port(self, port):
         if port is None:
             return
         try:
