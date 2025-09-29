c4dfdb52348e1287d7b9dac5c2e1cc8d68fae364
AswarS <62996133+AswarS@users.noreply.github.com>
2025-09-29 16:52:58 +0800

    Update convert_ckpt.sh
    rwkv

--- tools/checkpoint/rwkv/convert_ckpt.sh
+++ tools/checkpoint/rwkv/convert_ckpt.sh
@@ -1,3 +1,2 @@
-# python convert_ckpt.py <src_checkpoint.pt> <dst_checkpoint.pt> --direction ['ds2mg', 'mg2ds']
+# Usage: python convert_ckpt.py <src_checkpoint.pt> <dst_checkpoint.pt> --direction ['ds2mg', 'mg2ds']
 python convert_ckpt.py deepspeed_model.pt megatron_model.pth --direction ds2mg
-
