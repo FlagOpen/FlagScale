c782012d81df3731536a68351fdeac94b8978591
yanqingliu35-stack <yanqingliu35@gmail.com>
2025-11-04 11:51:15 +0800

    fix error

--- tools/patch/unpatch.py
+++ tools/patch/unpatch.py
@@ -276,13 +276,13 @@ def apply_hardware_patch(
 
             repo = Repo(submodule_path)
             try:
-                repo.git.apply(new_patch_file)
+                repo.git.apply("--whitespace", "warn", new_patch_file)
             except Exception as e:
                 logger.warning(
                     f"Failed to apply patch cleanly, and error is {e.stderr}. Retrying with --whitespace=fix."
                 )
 
-                repo.git.apply("--whitespace", "warn", new_patch_file)
+                repo.git.apply("--whitespace", "fix", new_patch_file)
             logger.info(f"Patch {new_patch_file} has been applied.")
 
         logger.info(f"Step 6: Moving patched temp path {temp_unpatch_path} to {final_path}")
